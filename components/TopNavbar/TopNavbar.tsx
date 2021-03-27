7355a29dfae6df6a38566c98e8bbe96f1ecba62d
Joaquin Peralta <joaquinperalta1406@gmail.com>
2021-03-27 16:58:37 -0300

    Responsive Topnavbar add

--- components/TopNavbar/TopNavbar.tsx
+++ components/TopNavbar/TopNavbar.tsx
@@ -1,10 +1,12 @@
 import { useContext } from 'react';
 import SidemenuContext from '@context/SidemenuContext';
 import SideshopbagContext from '@context/SideshopbagContext';
+import Link from 'next/link';
 import Image from 'next/image';
 import { IconContext } from 'react-icons';
 import { AiOutlineMenu, AiOutlineShopping } from 'react-icons/ai';
 import { colors } from '@utils/themes';
+import SidemenuData from '@components/Sidemenu/SidemenuData';
 
 const TopNavbar = () => {
   const [menuVisibility, setMenuVisibility] = useContext(SidemenuContext);
@@ -21,46 +23,136 @@ const TopNavbar = () => {
   const opacity = menuVisibility || shopVisibility ? '0.2' : '1';
 
   return (
-    <header className="header">
-      <IconContext.Provider value={{ color: '#fff', size: '24px' }}>
-        <button type="button" className="header-btn" onClick={showSidemenu}>
+    <IconContext.Provider value={{ color: '#fff', size: '24px' }}>
+      <header className="header">
+        <button type="button" className="menu-btn" onClick={showSidemenu}>
           <AiOutlineMenu />
         </button>
-        <div className="brand">
-          <Image src="/square-logo.png" alt="logo" width={64} height={64} />
-        </div>
-        <button type="button" className="header-btn" onClick={showSideshopbag}>
+        <Link href="/">
+          <a>
+            <div className="square-brand">
+              <Image src="/square-logo.png" alt="logo" width={72} height={72} />
+            </div>
+            <div className="brand">
+              <Image src="/logo.png" alt="logo" width={197} height={72} />
+            </div>
+          </a>
+        </Link>
+        <nav>
+          <ul className="list">
+            {SidemenuData.map((item) => (
+              <Link key={item.title} href={item.path}>
+                <a className="list-anchor">
+                  <li className="list-item">{item.title}</li>
+                </a>
+              </Link>
+            ))}
+          </ul>
+        </nav>
+        <button type="button" className="shop-btn" onClick={showSideshopbag}>
           <AiOutlineShopping />
         </button>
-      </IconContext.Provider>
 
-      <style jsx>{`
-        .header {
-          width: 100%;
-          height: 72px;
-          background-color: ${colors.primary};
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          position: fixed;
-          top: 0;
-          z-index: 1000;
-          opacity: ${opacity};
-        }
+        <style jsx>{`
+          .header {
+            width: 100%;
+            height: 72px;
+            background-color: ${colors.primary};
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            position: fixed;
+            top: 0;
+            z-index: 1000;
+            opacity: ${opacity};
+          }
 
-        .header-btn {
-          width: 72px;
-          height: 72px;
-          border: 0;
-          background-color: ${colors.primary};
-        }
+          .menu-btn {
+            width: 72px;
+            height: 72px;
+            border: 0;
+            background-color: ${colors.primary};
+          }
 
-        .brand {
-          width: 64px;
-          height: 64px;
-        }
-      `}</style>
-    </header>
+          .shop-btn {
+            width: 72px;
+            height: 72px;
+            border: 0;
+            background-color: ${colors.primary};
+          }
+
+          .square-brand {
+            width: 72px;
+            height: 72px;
+          }
+
+          .brand {
+            display: none;
+          }
+
+          nav {
+            display: none;
+          }
+
+          .list {
+            display: none;
+            list-style: none;
+            padding-left: 0;
+            margin-bottom: 0;
+            width: 100%;
+          }
+
+          .list-anchor {
+            text-decoration: none;
+            color: #fff;
+            font-style: italic;
+          }
+
+          .list-anchor:hover {
+            color: ${colors.analogous500};
+          }
+
+          .list-item {
+            display: inline-block;
+            padding: 1rem 1.5rem;
+          }
+
+          @media screen and (min-width: 768px) {
+            .square-brand {
+              display: none;
+            }
+
+            .brand {
+              display: block;
+              width: 197px;
+              height: 72px;
+            }
+          }
+
+          @media screen and (min-width: 992px) {
+            .header {
+              display: flex;
+              padding-left: 18px;
+            }
+            .menu-btn {
+              display: none;
+            }
+
+            nav {
+              display: block;
+              flex: 1;
+              text-align: end;
+              margin-right: 2rem;
+            }
+
+            .list {
+              display: block;
+              flex: 1;
+            }
+          }
+        `}</style>
+      </header>
+    </IconContext.Provider>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,9 @@
 import Head from 'next/head';
-import { useContext } from 'react';
-import SidemenuContext from '@context/SidemenuContext';
-import SideshopbagContext from '@context/SideshopbagContext';
-import { colors } from '@utils/themes';
+import GlobalStyles from '@styles/GlobalStyles';
 
 export default function Home() {
-  // eslint-disable-next-line no-unused-vars
-  const [menuVisibility, setMenuVisibility] = useContext(SidemenuContext);
-  // eslint-disable-next-line no-unused-vars
-  const [shopVisibility, setShopVisibility] = useContext(SideshopbagContext);
-
-  const backgroundColor =
-    menuVisibility || shopVisibility
-      ? `${colors.darkenBackground}`
-      : `${colors.background}`;
-
   return (
-    <div>
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -24,22 +11,7 @@ export default function Home() {
 
       <h1>Home</h1>
 
-      <style jsx global>{`
-        html {
-          box-sizing: border-box;
-        }
-        *,
-        *:before,
-        *:after {
-          box-sizing: inherit;
-        }
-        body {
-          margin: 0;
-          padding: 72px 0 0 0;
-          color: ${colors.darkgray};
-          background-color: ${backgroundColor};
-        }
-      `}</style>
-    </div>
+      <GlobalStyles />
+    </>
   );
 }
