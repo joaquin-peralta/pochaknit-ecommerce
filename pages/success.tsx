8836fc7f8d453c015dde318933f1101027fa6726
Joaquin Peralta <joaquinperalta1406@gmail.com>
2021-05-12 15:49:13 -0300

    Last commit for production

--- pages/success.tsx
+++ pages/success.tsx
@@ -107,7 +107,6 @@ export default function SuccessPage() {
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
-            text-align: center;
           }
         `}</style>
       </>
