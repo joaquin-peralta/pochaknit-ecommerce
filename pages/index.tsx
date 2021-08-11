2842a9cdb26769246df59471d9c3dc054c65012c
joaquin <joaquinperalta1406@gmail.com>
2021-08-11 09:42:56 -0300

    Added modal show for discounts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { useState, useEffect } from 'react';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import FeaturedPattern from '@components/FeaturedPattern';
 import { getStrapiUrl } from '@utils/strapi';
 import { Pattern } from '@types';
+
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 export const getStaticProps: GetStaticProps = async () => {
   const res = await fetch(getStrapiUrl('/patterns?is_featured=true&_sort=published_date:DESC'));
@@ -21,6 +25,14 @@ type Props = {
 };
 
 function Home({ patterns }: Props) {
+  const [modalShow, setModalShow] = useState(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setModalShow(true);
+    }
+  }, []);
+
   return (
     <>
       <Head>
@@ -34,6 +46,21 @@ function Home({ patterns }: Props) {
           </a>
         </Link>
       ))}
+
+      <Modal show={modalShow} size="lg" centered onHide={() => setModalShow(false)}>
+        <Modal.Body>
+          <h4 className="fw-bold">¡30% OFF en toda nuestra web!</h4>
+          <p>
+            ¡Aprovechá este descuento en todos nuestros patrones hasta el{' '}
+            <span className="fw-bold">18/08 inclusive</span>!
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => setModalShow(false)} className="fw-bold">
+            OK
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
